4d7a93dca1165770602449b7b3b6f47641bfa040
Luuppi <se.topsu@gmail.com>
2024-12-27 07:28:57 +0200

    added lfg-1 for historical reasons

---
+++ LFG-1.sh
@@ -0,0 +1,529 @@
+#!/bin/bash
+
+answer=1
+
+#variables 12A
+a1fi=''
+a2fi=""
+a3fi=""
+a4fi=""
+a5fi=""
+a6fi=""
+a7fi=""
+a8fi=""
+a9fi=""
+a10fi=""
+a11fi=""
+a12fi=""
+a13fi=""
+a14fi=""
+a15fi=""
+
+a1en=''
+a2en=""
+a3en=""
+a4en=""
+a5en=""
+a6en=""
+a7en=""
+a8en=""
+a9en=""
+a10en=""
+a11en=""
+a12en=""
+a13en=""
+a14en=""
+a15en=""
+
+
+
+
+
+
+
+
+while true
+do
+
+#teht 12a 1
+while [ $answer = 1 ]
+do
+echo
+echo 1 $a1en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a1fi)
+        echo correct
+        answer=2
+        break ;;
+    correct)
+        echo $a1fi
+        answer=1
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=1
+        echo ;;
+esac
+done
+
+
+
+
+
+
+
+#teht 12a2
+
+while [ $answer = 2 ]
+do
+echo
+echo 2 $a2en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a2fi)
+        echo correct
+        answer=3
+        break ;;
+    correct)
+        echo $a2fi
+        answer=2
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=2
+        echo ;;
+esac
+done
+
+
+
+
+
+
+
+#teht 12a3
+
+while [ $answer = 3 ]
+do
+echo
+echo 3 $a3en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a3fi)
+        echo correct
+        answer=4
+        break ;;
+    correct)
+        echo $a3fi
+        answer=3
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=3
+        echo ;;
+esac
+done
+
+
+
+
+
+#teht 12a4
+
+while [ $answer = 4 ]
+do
+echo
+echo 4 $a4en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a4fi)
+        echo correct
+        answer=5
+        break ;;
+    correct)
+        echo $a4fi
+        answer=4
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=4
+        echo ;;
+esac
+done
+
+
+
+
+#teht 12a5
+
+while [ $answer = 5 ]
+do
+echo
+echo 5 $a5en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a5fi)
+        echo correct
+        answer=6
+        break ;;
+    correct)
+        echo $a5fi
+        answer=5
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=5
+        echo ;;
+esac
+done
+
+
+
+
+
+#teht 12a6
+
+while [ $answer = 6 ]
+do
+echo
+echo 6 $a6en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a6fi)
+        echo correct
+        answer=7
+        break ;;
+    correct)
+        echo $a6fi
+        answer=6
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=6
+        echo ;;
+esac
+done
+
+
+
+
+
+#teht 12a7
+
+while [ $answer = 7 ]
+do
+echo
+echo 7 $a7en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a7fi)
+        echo correct
+        answer=8
+        break ;;
+    correct)
+        echo $a7fi
+        answer=7
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=7
+        echo ;;
+esac
+done
+
+
+
+
+
+#teht 12a8
+
+while [ $answer = 8 ]
+do
+echo
+echo 8 $a8en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a8fi)
+        echo correct
+        answer=9
+        break ;;
+    correct)
+        echo $a8fi
+        answer=8
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=8
+        echo ;;
+esac
+done
+
+
+
+
+
+
+#teht 12a9
+
+while [ $answer = 9 ]
+do
+echo
+echo 9 $a9en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a9fi)
+        echo correct
+        answer=10
+        break ;;
+    correct)
+        echo $a9fi
+        answer=9
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=9
+        echo ;;
+esac
+done
+
+
+
+
+
+
+#teht 12a10
+
+while [ $answer = 10 ]
+do
+echo
+echo 10 $a10en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a10fi)
+        echo correct
+        answer=11
+        break ;;
+    correct)
+        echo $a10fi
+        answer=10
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=10
+        echo ;;
+esac
+done
+
+
+
+
+#teht 12a11
+
+while [ $answer = 11 ]
+do
+echo
+echo 11 $a11en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a11fi)
+        echo correct
+        answer=12
+        break ;;
+    correct)
+        echo $a11fi
+        answer=11
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=11
+        echo ;;
+esac
+done
+
+
+
+
+
+
+#teht 12a12
+
+while [ $answer = 12 ]
+do
+echo
+echo 12 $a12en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a12fi)
+        echo correct
+        answer=13
+        break ;;
+    correct)
+        echo $a12fi
+        answer=12
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=12
+        echo ;;
+esac
+done
+
+
+
+
+
+#teht 12a13
+
+while [ $answer = 13 ]
+do
+echo
+echo 13 $a13en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a13fi)
+        echo correct
+        answer=14
+        break ;;
+    correct)
+        echo $a13fi
+        answer=13
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=13
+        echo ;;
+esac
+done
+
+
+
+
+
+#teht 12a14
+
+while [ $answer = 14 ]
+do
+echo
+echo 14 $a14en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a14fi)
+        echo correct
+        answer=15
+        break ;;
+    correct)
+        echo $a14fi
+        answer=14
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=14
+        echo ;;
+esac
+done
+
+
+
+
+
+
+#teht 12a15
+
+while [ $answer = 15 ]
+do
+echo
+echo 15 $a15en
+read -p "answer: " answer
+echo
+
+case $answer in
+    $a15fi)
+        echo correct
+        answer=1
+        break ;;
+    correct)
+        echo $a15fi
+        answer=15
+         ;;
+    1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)
+        echo
+        echo
+        break ;;
+    *) echo try again
+        answer=15
+        echo ;;
+esac
+done
+
+done
